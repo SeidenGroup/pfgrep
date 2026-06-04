d90ef0789e0caf85e2ab09f77b313347ae4f3c48
Calvin Buckley <calvin@cmpct.info>
2026-06-04 17:50:17 -0400

    also this will be zero length instead of a branch

--- pfgrep.cxx
+++ pfgrep.cxx
@@ -276,12 +276,11 @@ bool pfgrep::print_line(const File &file, const Match &match)
 		if (this->colourize == ColourizeAlways && match.substrings.size()) {
 			size_t last_substring_end = 0;
 			for (const auto& substring : match.substrings) {
-				if (substring.data() > match.line + last_substring_end) {
-					auto before = string_view(match.line + last_substring_end,
-						(substring.data() - match.line) - last_substring_end);
-					fmt::print("{}{}", maybe_colour(PFGREP_NORMAL_COLOUR), before);
-				}
-				fmt::print("{}{}", maybe_colour(PFGREP_MATCH_COLOUR), substring);
+				auto before = string_view(match.line + last_substring_end,
+					(substring.data() - match.line) - last_substring_end);
+				fmt::print("{}{}{}{}",
+					maybe_colour(PFGREP_NORMAL_COLOUR), before,
+					maybe_colour(PFGREP_MATCH_COLOUR), substring);
 				last_substring_end = (substring.data() - match.line) + substring.size();
 			}
 			auto after = string_view(match.line + last_substring_end,
